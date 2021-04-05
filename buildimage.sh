9773f2095367dd1c8f93f32db2db31e3977198f9
tsubiya <37928861+tsubiya@users.noreply.github.com>
2021-04-05 20:04:07 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -1,9 +1,9 @@
 
 version=$BUILD_ID
 docker build -t apprepo:$version .
-aws ecr get-login-password --region ap-south-1  | docker login --username AWS --password-stdin 202714190885.dkr.ecr.ap-south-1.amazonaws.com
-docker tag apprepo:$version 202714190885.dkr.ecr.ap-south-1.amazonaws.com/apprepo:$version
-docker push 202714190885.dkr.ecr.ap-south-1.amazonaws.com/apprepo:$version
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 454957204982.dkr.ecr.us-east-2.amazonaws.com
+docker tag apprepo:$version 454957204982.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
+docker push 454957204982.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
 
 
 
