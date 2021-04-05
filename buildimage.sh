9aef42798e279a21a1b5633df1afe4273dc16aed
tsubiya <37928861+tsubiya@users.noreply.github.com>
2021-04-06 01:19:18 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -10,3 +10,29 @@ docker push 454957204982.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version
 echo "Image Successfully pushed to ecr"
 
 
+
+
+#Create new task definition with new version of buildnumber
+
+
+
+
+
+
+
+sed -i s#202714190885.dkr.ecr.us-east-2.amazonaws.com/apprepo:20#202714190885.dkr.ecr.us-east-2.amazonaws.com/apprepo:$version#g taskdef.json
+
+
+aws ecs register-task-definition  --cli-input-json  file://taskdef.json --region us-east-2
+
+
+
+
+
+
+
+echo "New task revision created"
+
+
+
+
